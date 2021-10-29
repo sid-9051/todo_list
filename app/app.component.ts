2f6636fb3a5d9f46c9df29399e856e5cb930c373
sid-9051 <86563718+sid-9051@users.noreply.github.com>
2021-10-29 22:48:45 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'to-do';
+  today=Date.now();
+  
+  constructor()
+  {
+    // setTimeout(() =>{
+    //   this.title="Changed title";
+    // }, 2000)
+  }
+}
---
+++ app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ToDosComponent } from './Mycomponents/to-dos/to-dos.component';
+import { TodoItemComponent } from './Mycomponents/todo-item/todo-item.component';
+import { AddTodoComponent } from './Mycomponents/add-todo/add-todo.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ToDosComponent,
+    TodoItemComponent,
+    AddTodoComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
